52bfa381efaf3cb6b8427bb89db70cac5e33e118
vkrms <april504@yandex.ru>
2022-03-24 13:06:09 +0700

    feat: initial migration

---
+++ prisma/migrations/20220324055601_initial_migration/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "shared" BOOLEAN NOT NULL DEFAULT false,
+    "email" VARCHAR(255),
+    "token" VARCHAR(255),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "User_token_idx" ON "User"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_token_key" ON "User"("token");
