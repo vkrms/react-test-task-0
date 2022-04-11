118e07b43bd25f8883499672e6c5eb6acf87eb3b
vkrms <april504@yandex.ru>
2022-04-11 11:11:00 +0700

    refactor(api): update user

--- pages/api/update-user.js
+++ pages/api/update-user.js
@@ -3,23 +3,22 @@ import { PrismaClient } from '@prisma/client'
 export default function handler(req, res) {
   const prisma = new PrismaClient()
 
-  const { token, shared, email } = req.body
+  const { id, shared, email } = req.body
 
   async function main() {
     return await prisma.user.update({
-      where: { token },
+      where: { id },
       data: { shared, email },
     })
   }
 
   main()
     .then((updatedUser) => {
-      console.log({updatedUser})
-      res.status(200).json(updatedUser)
+      return res.status(200).json(updatedUser)
     })
     .catch(e => {
-      throw e
-      // TODO: return 400 for unique constraint failure
+      console.error(e)
+      return res.status(400).json({ message: e.message })
     })
     .finally( async () => {
       await prisma.$disconnect()
