aedc814fadbe59855227a9452be10daa406735ff
vkrms <april504@yandex.ru>
2022-04-11 11:11:58 +0700

    refactor: social component & use redux

--- components/Socials.js
+++ components/Socials.js
@@ -1,40 +1,26 @@
 import styles from './socials.module.scss'
-import { useDispatch } from 'react-redux'
-import { share } from './socialsSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { share } from '../redux/socialsSlice'
 import { updateUser } from '../redux/userSlice'
 import API from '../libs/api-calls'
 
-function openWindow(social, dispatch) {
+function openWindow(social, dispatch, id) {
   const popupX = window.innerWidth / 2 - 300
   const popupY = window.innerHeight / 2 - 300
-  const windowCfg = `height=600, width=600, top=${popupY}, left=${popupY}, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0`
+  const windowCfg = `height=600, width=600, top=${popupY}, left=${popupX}, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0`
 
-  let url = ''
-
-  switch (social) {
-    case 'vk':
-      url = 'https://vk.com/share.php?url='
-      break
-    case 'fb':
-      url = 'https://www.facebook.com/sharer/sharer.php?u='
-      break
-    case 'tw':
-      url = 'https://twitter.com/intent/tweet/?url='
-      break
-    case 'ok':
-      url = 'https://connect.ok.ru/offer?url='
-      break
-    default:
-      return false
-  }
+  const url = {
+    vk: 'https://vk.com/share.php?url=',
+    fb: 'https://www.facebook.com/sharer/sharer.php?u=',
+    tw: 'https://twitter.com/intent/tweet/?url=',
+    ok: 'https://connect.ok.ru/offer?url=',
+  }[social]
 
   const popup = window.open(url + window.location.href, 'share', windowCfg)
 
   // check if popup closed
   const closedInterval = setInterval(async () => {
     if (popup.closed) {
-      console.log('popup closed!')
-      const token = window.localStorage.getItem('testTaskToken')
       const user = await API.updateUser({
         id,
         shared: true,
@@ -44,7 +30,6 @@ function openWindow(social, dispatch) {
       clearInterval(closedInterval)
     }
   }, 100);
-
 }
 
 
@@ -52,6 +37,7 @@ export default function Socials() {
   const socials = ['vk', 'fb', 'tw', 'ok']
 
   const dispatch = useDispatch()
+  const id = useSelector(state => state.user.id)
 
   return (
     <div className="email">
@@ -60,7 +46,7 @@ export default function Socials() {
           <button
             className={`${styles.socials__btn} ${styles[social]}`}
             key={social}
-            onClick={() => { openWindow(social, dispatch) }}
+            onClick={() => { openWindow(social, dispatch, id) }}
           />
         )
       })}
